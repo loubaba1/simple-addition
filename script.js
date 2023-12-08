737307662099a860ffeb5cffff482ec4067287f8
loubaba1 <147740329+loubaba1@users.noreply.github.com>
2023-12-08 09:08:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// SIMPLE ADDITION IPO CALCULATOR
+// Input
+
+let number1 = +prompt("Enetr a number:");
+let number2 = +prompt("Enter another number:");
+//Process
+let total = number1 + number2;
+
+// Comment:
+//Output
+alert(` ${number1} + ${number2} = ${total}`);
